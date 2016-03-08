c6d51c8b09648c78e525ebf625430cbf69be5735
Jesse Panganiban <hello@jpanganiban.com>
2016-03-08 10:09:07 +0800

    Update initialize function.

--- src/initialize.py
+++ src/initialize.py
@@ -1,5 +1,7 @@
 import glob
 import os.path
+import serial
+import serial.tools.list_ports
 
 
 def get_modems():
@@ -7,4 +9,8 @@ def get_modems():
     modems = []
     for f in glob.glob('/dev/ttyACM*'):
         modems.append(f)
+
+    # If 'probably', windows
+    if not modems:
+        modems = [s[0] for s in serial.tools.list_ports.comports()]
     return modems
