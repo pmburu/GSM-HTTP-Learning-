0e3e4862571dcf22c84e2eb2830eb9e3fc08ed88
Jesse Panganiban <hello@jpanganiban.com>
2016-03-09 08:05:28 +0800

    Use ser_or_404 and pre-store serial connections.

--- src/server.py
+++ src/server.py
@@ -13,8 +13,9 @@ Serial = partial(serial.Serial, baudrate=SERIAL_BAUDRATE,
 print 'Initializing modem...'
 # Initialize modem phones.
 ports = {m: None for m in initialize.get_modems()}
-print initialize.get_modems()
 numbers = {}
+serials = {}
+unused_ports = []
 
 
 # Parse numbers.
@@ -22,9 +23,11 @@ for port in ports.keys():
     try:
         _ser = Serial(port)
     except:
+        unused_ports.append(port)
         continue
     number = serial_gsm.sim_msisdn(_ser)
     if number:
+        serials[number] = _ser
         ports[port] = number
         numbers[number] = port
 print 'Modem initialized!'
@@ -36,6 +39,12 @@ from flask import Flask, jsonify, abort, request
 app = Flask(__name__)
 
 
+def ser_or_404(number):
+    ser = serials.get(number, None)
+    if not ser:
+        return abort(404)
+
+
 def port_or_404(number):
     port = numbers.get(number, None)
     if not port:
@@ -44,59 +53,51 @@ def port_or_404(number):
 
 @app.route('/modems/<number>/prep_bal')
 def api_number_bal(number):
-    port = port_or_404(number)
+    ser = ser_or_404(number)
     return jsonify({'balance': 0})
 
 
 @app.route('/system/available_numbers')
 def api_available_numbers():
-    return jsonify({'numbers': numbers.keys()})
+    return jsonify({'numbers': serials.keys()})
 
 
 @app.route('/system/unused_ports')
 def api_unused_ports():
-    return jsonify({'ports': [p for p in ports.values() if p]})
+    return jsonify({'ports': unused_ports})
 
 
 @app.route('/modems/<number>/call', methods=['POST'])
 def api_call(number):
-    port = port_or_404(number)
+    ser = ser_or_404(number)
 
     dest_number = request.form['number']
     duration = int(request.form.get('duration', 0))
 
-    ser = Serial(port)
     res = serial_gsm.call(ser, dest_number, duration=duration)
     return jsonify(res)
 
 
 @app.route('/modems/<number>/wait_for_call', methods=['POST'])
 def api_wait_for_call(number):
-    port = port_or_404(number)
-
+    ser = ser_or_404(number)
     duration = int(request.form.get('duration', 0))
-
-    ser = Serial(port)
     res = serial_gsm.wait_and_answer_call(ser, duration=duration)
     return jsonify(res)
 
 
 @app.route('/modems/<number>/send_sms', methods=['POST'])
 def api_send_sms(number):
-    port = port_or_404(number)
-
+    ser = ser_or_404(number)
     recipient = request.form['number']
     message = request.form['message']
-
-    ser = Serial(port)
     res = serial_gsm.send_sms(ser, recipient, message)
     return jsonify(res)
 
 
 @app.route('/modems/<number>/inbox')
 def api_inbox(number):
-    port = port_or_404(number)
-    
+    ser = ser_or_404(number)
     return jsonify({
         'messages': serial_gsm.inbox_messages(ser)
     })
@@ -104,20 +105,15 @@ def api_inbox(number):
 
 @app.route('/modems/<number>/inbox', methods=['DELETE'])
 def api_clear_inbox(number):
-    port = port_or_404(number)
-
-    ser = Serial(port)
+    ser = ser_or_404(number)
     return jsonify(serial_gsm.delete_inbox_messages(ser))
 
 
 @app.route('/modems/<number>/ussd', methods=['POST'])
 def api_send_ussd(number):
-    port = port_or_404(number)
-
+    ser = ser_or_404(number)
     command = request.form['command']
     timeout = int(request.form.get('timeout', 0))
-
-    ser = Serial(port)
     res = serial_gsm.ussd_send(ser, command, timeout=timeout)
     return jsonify(res)
 
