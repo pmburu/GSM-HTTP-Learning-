046fb94c930ab706c257a05ae90f5cc0b26fc654
Jesse Panganiban <hello@jpanganiban.com>
2016-05-23 22:17:39 +0800

    Log USSD command.

--- src/serial_gsm.py
+++ src/serial_gsm.py
@@ -317,6 +317,7 @@ def ussd_send(ser, command, timeout=0):
     once the server has terminated the USSD session. This
     does not follow the provided timeout.
     """
+    print('USSD::Command: %s' % command)
     err, res = USSDSend(ser, command).run(timeout)
     return {
         'success': True,
